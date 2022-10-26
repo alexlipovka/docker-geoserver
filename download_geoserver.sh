80bbb8fdc1bccc777d29ba45ab341d79e3ff3513
Alex Lipovka <alex.lipovka@gmail.com>
2022-10-26 18:22:48 +0300

    mkdir war

--- download_geoserver.sh
+++ download_geoserver.sh
@@ -3,4 +3,5 @@
 mkdir ./temp
 wget https://build.geoserver.org/geoserver/main/geoserver-main-latest-war.zip -O ./temp/geoserver.war.zip
 unzip ./temp/geoserver.war.zip -d ./temp/
+mkdir ./war
 mv ./temp/geoserver.war ./war/
